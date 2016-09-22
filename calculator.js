ddcaf5907065894fbb547ff7b282f9caa8390db7
Michael Kühnel <m.kuehnel@micromata.de>
2016-09-22 17:17:02 +0200

    Updating font size when to many digits are entered

--- calculator.js
+++ calculator.js
@@ -3,26 +3,50 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	var buttons = document.querySelectorAll('button');
 
-	var addToOutput = function (value) {
-		var text = document.querySelectorAll('output')[0].textContent;
-		console.info('text', text);
-		console.info('typeof text', typeof text);
-		if (text === '0') {
-			document.querySelectorAll('output')[0].textContent = value;
-		} else {
-			document.querySelectorAll('output')[0].textContent = text + value;
+	var setFontSize = function (size) {
+		document.querySelectorAll('output')[0].style.fontSize = size + 'px';
+	};
+
+	var checkLength = function (output) {
+		var length = output.length;
+		if (length > 7) {
+			setFontSize('22');
 		}
+		if (length > 14) {
+			setFontSize('11');
+		}
+		if (length > 24) {
+			clearOutput();
+			addToOutput('TILT!!!');
+		}
+	};
+
+	var addToOutput = function (value) {
+		var output = document.querySelectorAll('output')[0].textContent;
+		// console.info('output', output);
+		// console.info('typeof output', typeof output);
+		if (output === 'TILT!!!') {
+			return;
+		}
+		if (output === '0') {
+			output = value;
+		} else {
+			output = output + value;
+		}
+		document.querySelectorAll('output')[0].textContent = output;
+		checkLength(output);
 	};
 
 	var clearOutput = function () {
 		document.querySelectorAll('output')[0].textContent = 0;
+		setFontSize('44');
 	}
 
 	for (var i = buttons.length; i--;) {
 		buttons[i].addEventListener('click', function (e) {
 			console.info(this.dataset);
 			if (this.dataset.value) {
-				addToOutput(Number(this.dataset.value));
+				addToOutput(this.dataset.value);
 			}
 			if (this.dataset.action === 'clear') {
 				clearOutput();
