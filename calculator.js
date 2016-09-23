714c49ef1d5ce4550cc85f55a4eb4eed964c9566
Michael Kühnel <m.kuehnel@micromata.de>
2016-09-23 12:33:00 +0200

    Implement addition

--- calculator.js
+++ calculator.js
@@ -2,7 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
 	'use strict';
 
 	var buttons = document.querySelectorAll('button');
+
+	var operators = {
+		add: function (a, b) {
+			return a + b;
+		}
+	};
+
 	var outputIsResult = false;
+	var operand1 = null;
+	var operand2 = null;
+	var operator = null;
+	var lastClick = null;
 
 	var checkLength = function (output) {
 		var length = String(output).length;
@@ -47,6 +58,9 @@ document.addEventListener('DOMContentLoaded', function () {
 	var clearOutput = function () {
 		renderOutput(0);
 		outputIsResult = false;
+		operand1 = null;
+		operand2 = null;
+		operator = null;
 	}
 
 	var changeSign = function () {
@@ -71,24 +85,66 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	};
 
+	var addition = function () {
+		console.info('addition');
+		outputIsResult = true;
+		operator = 'add';
+		if (operand1) {
+			operand2 = Number(getInput());
+			renderOutput(operators[operator](operand1, operand2));
+			operand1 = Number(getInput());
+		} else {
+			operand1 = Number(getInput());
+		}
+	};
+
+	var result = function () {
+		operand2 = Number(getInput());
+		renderOutput(operators[operator](operand1, operand2));
+		outputIsResult = true;
+		operand1 = Number(getInput());
+	};
+
 	for (var i = buttons.length; i--;) {
 		buttons[i].addEventListener('click', function (e) {
 			console.info(this.dataset);
 			if (this.dataset.number) {
 				addToOutput(this.dataset.number);
+				lastClick = 'number';
 			}
 			if (this.dataset.action === 'clear') {
 				clearOutput();
+				lastClick = 'action';
 			}
 			if (this.dataset.action === 'sign') {
 				changeSign();
+				lastClick = 'action';
 			}
 			if (this.dataset.action === 'percent') {
 				percentage();
+				lastClick = 'action';
 			}
 			if (this.dataset.action === 'float') {
 				makeFloat(e);
+				lastClick = 'action';
 			}
+			if (this.dataset.action === 'add') {
+				if (lastClick === 'action') {
+					e.target.blur();
+					return;
+				}
+				addition();
+				lastClick = 'action';
+			}
+			if (this.dataset.action === 'equal') {
+				if (lastClick === 'action') {
+					e.target.blur();
+					return;
+				}
+				result();
+				lastClick = 'action';
+			}
+			e.target.blur();
 		});
 	}
 
